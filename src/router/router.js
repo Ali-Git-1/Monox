5238fd26915a29f6ebc0ece6cf7dc0a700814609
Ali Arabpour <alinima8590@gmail.com>
2026-09-25 13:36:08 +0330

    fix: use createWebHashHistory for github pages routing

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import HomePage from "../views/HomePage.vue";
 import ProductsPage from "@/views/ProductsPage.vue";
@@ -114,7 +114,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0, behavior: "smooth" };
